582b28aa022d85a1524f064dcc6f2dd6594b2648
Brad Oyler <bradoyler@gmail.com>
2017-07-28 00:27:31 -0400

    formatting

--- TimeTrigger2/index.js
+++ TimeTrigger2/index.js
@@ -1,11 +1,9 @@
 module.exports = function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
+  var timeStamp = new Date().toISOString()
 
-    if(myTimer.isPastDue)
-    {
-        context.log('JavaScript is running late!');
-    }
-    context.log('TimeTrigger2 function ran!', timeStamp);
-
-    context.done();
-};
+  if (myTimer.isPastDue) {
+    context.log('JavaScript is running late!')
+  }
+  context.log('TimeTrigger2 function ran!', timeStamp)
+  context.done()
+}
